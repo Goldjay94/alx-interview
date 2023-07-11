2ab514a671b860f4857341fef12077d8eefdb9bc
Goldjay94 <genevieveezeh786@gmail.com>
2023-07-11 21:50:13 +0000

    0. Minimum Operations updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-Minimum Operations module"""
+
+'''
+Given a number n, write a method that calculates
+the fewest number of operations needed to result in
+exactly n H characters in the file.
+'''
 
 
 def minOperations(n):
-    '''
-    returns the minimum operations to get n H's
-    '''
-    min_operations = 0
+    '''Returns min operations to get n Hs.'''
 
-    if n <= 1:
-        return min_operations
-
-    for i in range(2, n + 1):
-        while n % i == 0:
-            min_operations += i
-            n /= i
-
-    return min_operations
-
-
-if __name__ == '__main__':
-    from random import randint
-    from time import time
-
-    start_time = time()
-
-    for i in range(10):
-        n = randint(2, 100)
-        print("Min # of operations to reach {} char: {}".
-              format(n, minOperations(n)))
-
-    print(f'==> Program completed in {time() - start_time:.3f}s')
+    result = 0
+    index = 2
+    if n < 2:
+        return 0
+    while (index < n + 1):
+        while n % index == 0:
+            result += index
+            n = n / index
+        index += 1
+    return result
