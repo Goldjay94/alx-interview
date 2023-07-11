a6843868fa9b823f214d4cb292c5bebda1efc171
Goldjay94 <genevieveezeh786@gmail.com>
2023-07-11 21:34:24 +0000

    0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Minimum Operations module"""
+
+
+def minOperations(n):
+    '''
+    returns the minimum operations to get n H's
+    '''
+    min_operations = 0
+
+    if n <= 1:
+        return min_operations
+
+    for i in range(2, n + 1):
+        while n % i == 0:
+            min_operations += i
+            n /= i
+
+    return min_operations
+
+
+if __name__ == '__main__':
+    from random import randint
+    from time import time
+
+    start_time = time()
+
+    for i in range(10):
+        n = randint(2, 100)
+        print("Min # of operations to reach {} char: {}".
+              format(n, minOperations(n)))
+
+    print(f'==> Program completed in {time() - start_time:.3f}s')
