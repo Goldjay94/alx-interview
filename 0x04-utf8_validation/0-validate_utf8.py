39f96c086743e5cbd2a9d9ac0bb9ed6e1dacd19b
Goldjay94 <genevieveezeh786@gmail.com>
2023-07-25 19:09:49 +0000

    error fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,49 +1,26 @@
 #!/usr/bin/python3
 """Module for validUtf8"""
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
 
 
 def validUTF8(data):
-    """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
+    """Check if a list of integers is a valid utf-8 set"""
+    nbytes = 0
 
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
+    byte1 = 1 << 7
+    byte2 = 1 << 6
 
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
+    for number in data:
+        b = 1 << 7
+        if nbytes == 0:
+            while b & number:
+                nbytes += 1
+                b = b >> 1
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
                 return False
-            if byte[0:2] != [1, 0]:
+        else:
+            if not (number & byte1 and not (number & byte2)):
                 return False
-    return True
+        nbytes -= 1
+    return nbytes == 0
